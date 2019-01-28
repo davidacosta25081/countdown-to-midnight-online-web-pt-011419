4f544d010e369f245626c120a4000808bad4f4fa
David Acosta <davidacosta25081@yahoo.com>
2019-01-28 21:40:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
 
-def countdown
+
+def countdown(counter)
+
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+"HAPPY NEW YEAR!  "
+
 end
+
+countdown(30)
