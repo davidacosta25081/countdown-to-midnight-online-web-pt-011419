79ccac0e1ab890cab80981a41b0d7061f7b008dd
David Acosta <davidacosta25081@yahoo.com>
2019-01-28 21:43:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,19 @@ def countdown(counter)
 end
 
 countdown(30)
+
+
+
+def countdown_with_sleep(counter)
+
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+sleep 1
+  end
+"HAPPY NEW YEAR!"
+
+end
+
+
+
